3d3c02b3b00baba767992230f5f7034ab8392daf
Frank Dillon <dillonf2@outlook.com>
2023-07-13 19:30:24 -0400

    python ORMs lab complete

--- lib/dog.py
+++ lib/dog.py
@@ -4,5 +4,71 @@ CONN = sqlite3.connect('lib/dogs.db')
 CURSOR = CONN.cursor()
 
 class Dog:
-    
-    pass
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+        self.id = None
+
+    @classmethod
+    def create_table(cls):
+        CURSOR.execute("""
+            CREATE TABLE IF NOT EXISTS dogs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                breed TEXT
+            )
+        """)
+
+    @classmethod
+    def drop_table(cls):
+        CURSOR.execute("DROP TABLE IF EXISTS dogs")
+
+    def save(self):
+        CURSOR.execute("INSERT INTO dogs (name, breed) VALUES (?, ?)", (self.name, self.breed))
+        self.id = CURSOR.lastrowid
+        CONN.commit()
+
+    @classmethod
+    def create(cls, name, breed):
+        dog = cls(name, breed)
+        dog.save()
+        return dog
+
+    @classmethod
+    def new_from_db(cls, row):
+        dog = cls(row[1], row[2])
+        dog.id = row[0]
+        return dog
+
+    @classmethod
+    def get_all(cls):
+        CURSOR.execute("SELECT * FROM dogs")
+        rows = CURSOR.fetchall()
+        return [cls.new_from_db(row) for row in rows]
+
+    @classmethod
+    def find_by_name(cls, name):
+        CURSOR.execute("SELECT * FROM dogs WHERE name=?", (name,))
+        row = CURSOR.fetchone()
+        if row:
+            return cls.new_from_db(row)
+        return None
+
+    @classmethod
+    def find_by_id(cls, id):
+        CURSOR.execute("SELECT * FROM dogs WHERE id=?", (id,))
+        row = CURSOR.fetchone()
+        if row:
+            return cls.new_from_db(row)
+        return None
+
+    @classmethod
+    def find_or_create_by(cls, name, breed):
+        dog = cls.find_by_name(name)
+        if dog:
+            return dog
+        return cls.create(name, breed)
+
+    def update(self):
+        CURSOR.execute("UPDATE dogs SET name=?, breed=? WHERE id=?", (self.name, self.breed, self.id))
+        CONN.commit()
